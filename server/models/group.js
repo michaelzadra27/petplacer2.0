066d9261b0a2a3c78abdd00f5ccf0497c20659a9
Trhines <trhines94@gmail.com>
2021-09-04 15:44:20 -0700

    resolvers skeleton added

--- server/models/group.js
+++ server/models/group.js
@@ -3,9 +3,14 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const groupSchema = new Schema({
+  groupName: {
+    type: String,
+    trim: true,
+    required: 'Group must have a name of type string'
+  },
   users: {
     type: Array
-}
+  },
 });
 
 const Group = mongoose.model("Group", groupSchema);
