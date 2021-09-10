3e530d570d6c1a26c74828d70534f507af402b26
michaelzadra27 <michaelzadra27@gmail.com>
2021-09-09 20:49:48 -0700

    wrote state functionality for pet card rendering data from api and changing with state onclick

--- client/src/components/main/navbar2.js
+++ client/src/components/main/navbar2.js
@@ -11,82 +11,156 @@ const selections = {};
 var apiData = []
 var dogs = apiData[0]
 
-function getData() {
-    const getToken = async () => {
-        const params = new URLSearchParams();
-        params.append("grant_type", "client_credentials");
-        params.append("client_id", key);
-        params.append("client_secret", secret);
-        const petRes = await fetch(
-            "https://api.petfinder.com/v2/oauth2/token",
-            {
-                method: "POST",
-                body: params,
-            }
-        );
-        const data = await petRes.json();
-        apiCall(data.access_token)
-    };
-    getToken();
-    function apiCall(access_token) {
-        //Conditionally Call the API based on if there was a sex selected or not
-        if (selections.sex === "Any") {
-            fetch(
-                `https://api.petfinder.com/v2/animals?type=dog&breed=${selections.breed}&location=${selections.location}`,
-                {
-                    headers: {
-                        Authorization: `Bearer ${access_token}`,
-                    },
-                }
-            )
-                .then(function (response) {
-                    return response.json();
-                })
-                .then(function (data1) {
-                    console.log(data1)
-                })
-        }
-        else {
-            fetch(
-                `https://api.petfinder.com/v2/animals?type=dog&breed=${selections.breed}&sex=${selections.sex}&location=${selections.location}`,
-                {
-                    headers: {
-                        Authorization: `Bearer ${access_token}`,
-                    },
-                }
-            )
-                .then(function (response) {
-                    return response.json();
-                })
-                .then(function ({ animals }) {
-                    // apiData.push(data1)
-                    apiData.push(animals)
-                    // var dogs = apiData[0]
-                    console.log("LLLLLLLL")
-                    console.log(dogs)
-                    var start = Math.floor(Math.random() * dogs.length)
-                    console.log(start)
-                })
-        };
-    };
-}
+// function getData() {
+//     const getToken = async () => {
+//         const params = new URLSearchParams();
+//         params.append("grant_type", "client_credentials");
+//         params.append("client_id", key);
+//         params.append("client_secret", secret);
+//         const petRes = await fetch(
+//             "https://api.petfinder.com/v2/oauth2/token",
+//             {
+//                 method: "POST",
+//                 body: params,
+//             }
+//         );
+//         const data = await petRes.json();
+//         apiCall(data.access_token)
+//     };
+//     getToken();
+//     function apiCall(access_token) {
+//         //Conditionally Call the API based on if there was a sex selected or not
+//         if (selections.sex === "Any") {
+//             fetch(
+//                 `https://api.petfinder.com/v2/animals?type=dog&breed=${selections.breed}&location=${selections.location}`,
+//                 {
+//                     headers: {
+//                         Authorization: `Bearer ${access_token}`,
+//                     },
+//                 }
+//             )
+//                 .then(function (response) {
+//                     return response.json();
+//                 })
+//                 .then(function (data1) {
+//                     console.log(data1)
+//                 })
+//         }
+//         else {
+//             fetch(
+//                 `https://api.petfinder.com/v2/animals?type=dog&breed=${selections.breed}&sex=${selections.sex}&location=${selections.location}`,
+//                 {
+//                     headers: {
+//                         Authorization: `Bearer ${access_token}`,
+//                     },
+//                 }
+//             )
+//                 .then(function (response) {
+//                     return response.json();
+//                 })
+//                 .then(function ({ animals }) {
+//                     // apiData.push(data1)
+//                     apiData.push(animals)
+//                     // var dogs = apiData[0]
+//                     console.log(apiData)
+//                     var dogs = apiData[0];
+//                     var start = Math.floor(Math.random() * dogs.length)
+//                     console.log(start)
+                    
+//                 })
 
-// const cycleCards = () => {
-
-//     dogID = dogs[i].id
-  
-//     dogoPhoto = data1.animals[i].photos[0].large
-    
-//     dogName = data1.animals[i].name
-
+//         };
+//     };
 // }
+
 
 
 const Navbar2 = () => {
 
+    function getData() {
+        const getToken = async () => {
+            const params = new URLSearchParams();
+            params.append("grant_type", "client_credentials");
+            params.append("client_id", key);
+            params.append("client_secret", secret);
+            const petRes = await fetch(
+                "https://api.petfinder.com/v2/oauth2/token",
+                {
+                    method: "POST",
+                    body: params,
+                }
+            );
+            const data = await petRes.json();
+            apiCall(data.access_token)
+        };
+        getToken();
+        function apiCall(access_token) {
+            //Conditionally Call the API based on if there was a sex selected or not
+            if (selections.sex === "Any") {
+                fetch(
+                    `https://api.petfinder.com/v2/animals?type=dog&breed=${selections.breed}&location=${selections.location}`,
+                    {
+                        headers: {
+                            Authorization: `Bearer ${access_token}`,
+                        },
+                    }
+                )
+                    .then(function (response) {
+                        return response.json();
+                    })
+                    .then(function (data1) {
+                        console.log(data1)
+                    })
+            }
+            else {
+                fetch(
+                    `https://api.petfinder.com/v2/animals?type=dog&breed=${selections.breed}&sex=${selections.sex}&location=${selections.location}`,
+                    {
+                        headers: {
+                            Authorization: `Bearer ${access_token}`,
+                        },
+                    }
+                )
+                    .then(function (response) {
+                        return response.json();
+                    })
+                    .then(function ({ animals }) {
+                        // apiData.push(data1)
+                        apiData.push(animals)
+                        // var dogs = apiData[0]
+                        console.log(apiData)
+                        var dogs = apiData[0];
+                        var start = Math.floor(Math.random() * dogs.length)
+                        RenderFirst(start)
+                        
+                    })
+    
+            };
+        };
+    }
+    
+   const RenderFirst = (start) => {
+    console.log(start + " HERE")
+    var dogs = apiData[0];
+    //Randomly start at one of the dogs returned
+    
+    var dogIdApi = dogs[start].id
+    var dogPhotoApi = dogs[start].photos[0].large
+    var dogNameApi = dogs[start].name
+    console.log(dogs[0])
+    console.log(dogPhotoApi)
+    console.log(dogIdApi)
+    setName(dogNameApi) 
+   }
+
+    //Setting up State to update NavBar with selections
     const [dogBreed, setBreed] = useState('Breed');
     const [dogSex, setSex] = useState('Sex');
     const [dogLocation, setLocation] = useState('Location');
+
+    const [dogName, setName] = useState('Dog Name');
+    const [dogImage, setImage] = useState()
+
     //Function handlers for the different dropdowns to get selected values into object
     const handleBreed = (e) => {
         console.log(e)
@@ -107,6 +181,23 @@ const Navbar2 = () => {
         console.log(selections)
         setLocation(e);
     };
+
+
+    const CycleCards = () => {
+    var dogs = apiData[0];
+    console.log(dogs.length)
+    //Randomly start at one of the dogs returned
+    var i = Math.floor(Math.random() * dogs.length)
+    var dogIdApi = dogs[i].id
+    var dogPhotoApi = dogs[i].photos[0].large
+    var dogNameApi = dogs[i].name
+
+    
+    setImage(dogPhotoApi)
+    setName(dogNameApi)
+    
+    
+    }
 
     return (
         <div>
@@ -184,19 +275,22 @@ const Navbar2 = () => {
                     <button className="search-button" onClick={getData}>Search</button>
                 </div>
             </div>
-            
-            
+
+            {/* Pet Display Cards */}
             <div className="petDisplay">
                 <div className="card">
                     <h2>Pet Matches Go Here</h2>
+                    <p className="petName">{dogName}</p>
+                    <img src={dogImage} alt="dog" className="petImagedsss"/>
+
                 </div>
                 <div className="selectionBtns">
                     <button className="no-button">Boo</button>
-                    <button className="yes-button">Yay</button>
+                    <button className="yes-button" onClick={CycleCards}>Yay</button>
                 </div>
             </div>
         </div>
-        
+
 
 
     );
