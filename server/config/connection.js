eaa381201f1e934e1749af59b36bacbd8b10be90
michaelzadra27 <michaelzadra27@gmail.com>
2021-09-11 21:20:53 -0700

    server changes

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,7 +3,9 @@ require('dotenv').config();
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/petplacer2', {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
 });
 
 module.exports = mongoose.connection;
