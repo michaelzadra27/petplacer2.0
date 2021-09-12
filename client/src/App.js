41f320585dad0c55c02358b49e64a22b794c9e09
michaelzadra27 <michaelzadra27@gmail.com>
2021-09-11 21:48:46 -0700

    server try again cmon man

--- client/src/App.js
+++ client/src/App.js
@@ -50,7 +50,7 @@ const client = new ApolloClient({
       },
     });
   },
-  uri: 'https://petplacer2.herokuapp.com//graphql',
+  uri: '/graphql',
   cache: new InMemoryCache(),
 });
 console.log(process.env)
