7a7669182f69994b87cd1c550dd5fa9d9efe2d81
michaelzadra27 <michaelzadra27@gmail.com>
2021-09-14 16:49:49 -0700

    adding static data to work with

--- client/src/components/main/mymatches.js
+++ client/src/components/main/mymatches.js
@@ -1,24 +1,57 @@
 import "./mymatches.css"
 import Logo from "../../../src/petplacer2.png"
 
+var dogName = "Ruben"
+var dogPhotoApi = "https://dl5zpyw5k3jeb.cloudfront.net/photos/pets/52506384/5/?bust=1631388396&width=600"
+var description = "Cute and playful little guy. Ready for their forever home. \n\nTo adopt please apply online at foac.us"
+var dogURL = "https://www.petfinder.com/dog/cheyenne-52978230/ca/madera/madera-county-animal-control-ca628/?referrer_id=c214bc92-08ab-4f73-985b-d06bda77705e"
+var contactEmail = "AnimalControl2@Madera-County.com"
+var contactCity = "Sacramento"
+
 const MyMatches = () => {
-    return ( 
+
+    // const [dogPhotoApi, setPhotoApi] = useState('')
+// const [dogURL, setDogURL] = useState('')
+// const [contactEmail, setEmail] = useState('')
+// const [dogName, setName] = useState('Dog Name');
+// const [description, setDescription] = useState('')
+// const [contactCity, setCity] = useState('')
+    return (
         <div className="myMatches">
-        <div className="matchCard">
-        <h4>My Matches</h4>
-        <div className="matchedPets">
-            <div>
-            <img className="smallMatchCard" src={Logo} alt="Pet Placer Logo"></img>
+            <h4>My Matches</h4>
+            <div className="matchCard">
+                <h3 className="likedInfo">{dogName}</h3>
+                <div className="matchedPets">
+                    <div>
+                        <img className="smallMatchCard" src={dogPhotoApi}></img>
+                        <p className="desc">{description}</p>
+                    </div>
+                </div>
             </div>
-            <p className="matchedInfo">"dogName" dynamic input - MATCHES</p>
+            <div className="matchCard">
+                <h3 className="likedInfo">{dogName}</h3>
+                <div className="matchedPets">
+                    <div>
+                        <img className="smallMatchCard" src={dogPhotoApi}></img>
+                        <p className="desc">{description}</p>
+                    </div>
+                </div>
+            </div>
+            <div className="matchCard">
+                <h3 className="likedInfo">{dogName}</h3>
+                <div className="matchedPets">
+                    <div>
+                        <img className="smallMatchCard" src={dogPhotoApi}></img>
+                        <p className="desc">{description}</p>
+                    </div>
+                </div>
+            </div>
+            <div className="selections">
+                <button className="back-button match">Back</button>
+            </div>
+
         </div>
-    </div>
-    <div className="selections">
-        <button className="back-button match">Back</button>
-    </div>
-    
-</div>
-     );
+    );
 }
- 
+
 export default MyMatches;
