188ab5236363636c024d12b776852310088ea81f
michaelzadra27 <80440959+michaelzadra27@users.noreply.github.com>
2021-09-15 16:34:17 -0700

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -49,8 +49,8 @@ const client = new ApolloClient({
       }
     })
   },
-  uri: 'http://localhost:3001/graphql',
-  // uri: 'https://petplacer2.herokuapp.com/graphql',
+  uri: 'https://petplacer2.herokuapp.com/graphql',
+
 
 })
 
